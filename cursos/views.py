200b437cb386aef2857e2e45621d5e1dd2433527
matheuslopes <matheushl1996@gmail.com>
2020-03-30 12:08:42 -0300

    Initial commit

---
+++ cursos/views.py
@@ -0,0 +1,19 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .models import Curso, Avaliacao
+from .serializers import CursoSerializer, AvaliacaoSerializer
+
+
+class CursoAPIView(APIView):
+    def get(self, request):
+        cursos = Curso.objects.all()
+        serializer = CursoSerializer(cursos, many=True)
+        return Response(serializer.data)
+
+
+class AvaliacaoAPIView(APIView):
+    def get(self, request):
+        avaliacoes = Avaliacao.objects.all()
+        serializer = AvaliacaoSerializer(avaliacoes, many=True)
+        return Response(serializer.data)
